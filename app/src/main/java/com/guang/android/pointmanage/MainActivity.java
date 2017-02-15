3435ec045905426578f9dc74fd6aed28bf588c79
许度庆 <xu.duqing@gmail.com>
2017-02-15 14:53:20 +0800

    first commit project init

---
+++ app/src/main/java/com/guang/android/pointmanage/MainActivity.java
@@ -0,0 +1,13 @@
+package com.guang.android.pointmanage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
